f196b7b98c7000cac6dfb87fa6beeaa7e09f643f
ninjaparade <yazjallad@gmail.com>
2023-03-25 13:16:07 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftTailwindColors
+
+A description of this package.
---
+++ Sources/SwiftTailwindColors/SwiftTailwindColors.swift
@@ -0,0 +1,6 @@
+public struct SwiftTailwindColors {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
